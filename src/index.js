56c10a61c681022dccd027cb831b37bae9fffa9d
AmsterGet <amsterget@gmail.com>
2018-03-02 19:13:41 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+    const resultsArray = calculateZeroes(number, base);
+
+    let tailingZeroesCount = resultsArray[0];
+    resultsArray.forEach((item) => {
+        if (item<tailingZeroesCount) {
+            tailingZeroesCount = item;
+        }
+    });
+    return tailingZeroesCount;
+};
+
+const getBasePowers = function getBasePrimePowers(baseNumber) {
+    let j = 1;
+    let i = 2;
+    let basePrimePowersArray = [];
+    while (i < baseNumber) {
+        if (baseNumber % i === 0) {
+            basePrimePowersArray[j] = i;
+            j++;
+            baseNumber = baseNumber / i;
+        }
+        else {
+            i++;
+        }
+    }
+    basePrimePowersArray[j] = i;
+
+    const basePrimePowers = {};
+    basePrimePowersArray.forEach((item) => {
+        if (!basePrimePowers[item]) {
+            basePrimePowers[item] = 1;
+        } else {
+            basePrimePowers[item]++;
+        }
+    });
+
+    return basePrimePowers;
+};
+
+const calculateZeroes = function calculateZeroesForAllPrimePowers(number, base) {
+    const basePrimePowers = getBasePowers(base);
+    let iterationNumber = number;
+    let rezArray = [];
+
+    for (let item in basePrimePowers) {
+        let itemSum = 0;
+        let itemAccumulator = +item;
+        while (itemAccumulator<=iterationNumber) {
+            itemSum+=Math.floor(iterationNumber/itemAccumulator);
+            itemAccumulator*=+item;
+        }
+        itemSum = Math.floor(itemSum/basePrimePowers[item]);
+        rezArray.push(itemSum);
+    }
+    return rezArray;
+};
